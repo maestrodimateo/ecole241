308515ce9fff2cf50ea6abb4c91dd02e39fdb81e
mebale <noelmeb12@gmail.com>
2019-02-16 04:56:16 +0100

    ajout des pages

---
+++ totale/totalitaire/js/script.js
@@ -0,0 +1,35 @@
+
+
+// Or with jQuery
+$(document).ready(function () {
+
+  $('.collapsible').collapsible();
+  $('.tooltipped').tooltip();
+  $('select').formSelect();
+  $(".dropdown-trigger").dropdown();
+  $('.sidenav').sidenav();
+  $('.tabs').tabs();
+  $('.materialboxed').materialbox();
+  $('.materialboxed').materialbox();
+  $('.modal').modal();
+  $("#search-icon").click(function (e) {
+    $("#search-icon").hide();
+    $("#search-form").show();
+    $("#search-txt").focus();
+    $("#logo").hide();
+    $("#menu").hide();
+  });
+  $("#close").click(function (e) {
+    $("#search-icon").show();
+    $("#search-form").hide();
+    $("#logo").show();
+    $("#menu").show();
+  });
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Akanda , Gabon": null,
+      "Libreville, Gabon": null,
+      "Owendo, Gabon": null,
+    },
+  });
+});  

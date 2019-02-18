a6d9fc74cb47ef03381815cc2b9e81eab97fe284
mebale <noelmeb12@gmail.com>
2019-02-18 14:31:03 +0100

    mise a jour de s5

--- sprint/s5.js
+++ sprint/s5.js
@@ -2,4 +2,4 @@ var nbres = [];
 for (let index = 1; index <= 2; index++) {
     nbres.push(Number.parseInt(prompt(`Donnez le nombre ${index}`)));
 }
-console.log((nbres[0] > nbres[1]) ? nbres[0] : nbres[1]);
+console.log(nbres.sort().reverse()[0]);

080efa2a610af6be72e494ae8c9e5cad7816e759
mebale <noelmeb12@gmail.com>
2019-02-18 15:33:16 +0100

    mise a jour de s7

--- sprint/s7.js
+++ sprint/s7.js
@@ -1,5 +1,5 @@
-let nbre = Number.parseFloat(prompt("Veuillez saisir un nombre negatif"))
+let nbre = prompt("Veuillez saisir un nombre negatif")
 while (nbre > 0 || Number.isNaN(nbre)) {
-    nbre = Number.parseFloat(prompt("Erreur! saisissez un nombre negatif hein!!!"));
+    nbre = prompt("Erreur! saisissez un nombre negatif hein!!!");
 }
 console.log("Well done my friend");

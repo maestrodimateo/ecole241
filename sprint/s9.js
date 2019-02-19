dd75430089be241a46952d02551b90ee6c54af85
mebale <noelmeb12@gmail.com>
2019-02-19 05:43:31 +0100

    update s9

--- sprint/s9.js
+++ sprint/s9.js
@@ -1,5 +1,6 @@
-let max = 0, nbre;
-for (let i = 1; i <= 5; i++) {
+let max, nbre;
+max = + prompt("inserez le nombre 1");
+for (let i = 2; i <= 5; i++) {
     nbre = + prompt(`Insérez le nombre ${i}`);
     if (nbre > max) {
         max = nbre;

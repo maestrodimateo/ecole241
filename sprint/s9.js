ef27f61255c9ab06b8ceb150ced1e6e81817147b
mebale <noelmeb12@gmail.com>
2019-02-21 10:44:32 +0100

    fix last sidebar

--- sprint/s9.js
+++ sprint/s9.js
@@ -1,5 +1,5 @@
 let max, nbre;
-max = + prompt("inserez le nombre 1");
+max = + prompt("Insérez le nombre 1");
 for (let i = 2; i <= 5; i++) {
     nbre = + prompt(`Insérez le nombre ${i}`);
     if (nbre > max) {

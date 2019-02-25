4aef5a9984028f1f34d97458c3dfaf2907b1e11f
mebale <noelmeb12@gmail.com>
2019-02-25 16:36:32 +0100

    refactor s9

--- sprint/s9.js
+++ sprint/s9.js
@@ -1,9 +1,5 @@
-let max, nbre;
-max = + prompt("Insérez le nombre 1");
-for (let i = 2; i <= 5; i++) {
-    nbre = + prompt(`Insérez le nombre ${i}`);
-    if (nbre > max) {
-        max = nbre;
-    }
+let nbres = [];
+for (let i = 1; i <= 5; i++) {
+    nbres.push(+ prompt(`Insérez le nombre ${i}`));
 }
-console.log(`Le plus grand est : ${max}`);
+console.log(`Le plus grand est : ${Math.max(...nbres)}`);

ae6beec8bbdedfa5830a6531762f7b7da50540e1
mebale <noelmeb12@gmail.com>
2019-03-02 10:41:05 +0100

    ajout donapen

--- sprint/s9.js
+++ sprint/s9.js
@@ -1,5 +1,3 @@
 let nbres = [];
-for (let i = 1; i <= 5; i++) {
-    nbres.push(+ prompt(`Insérez le nombre ${i}`));
-}
+for (let i = 1; i <= 5; i++) nbres.push(+ prompt(`Insérez le nombre ${i}`));
 console.log(`Le plus grand est : ${Math.max(...nbres)}`);

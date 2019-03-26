4fdeadb5d6840711f96ebfde6ca35acf384c9d22
mebale <noelmeb12@gmail.com>
2019-03-26 15:04:06 +0100

    creation des variables acte2.php

--- sprint/php/acte2.php
+++ sprint/php/acte2.php
@@ -1,11 +1,19 @@
 
-<?php $tab = array('nom' => 'noel'); ?>
+<?php 
+    $tab = array('nom' => 'noel');
+    $prenom = 'noel';
+    $nbre = 1;
+    $float = 1.5;
+    $bool = true;
+    $null = null;
+    $obj = new stdClass;
+?>
 <ul>
-    <li><?php var_dump('noel') ?></li>
-    <li><?php var_dump(1) ?></li>
-    <li><?php var_dump(1.5) ?></li>
-    <li><?php var_dump(true) ?></li>
+    <li><?php var_dump($prenom) ?></li>
+    <li><?php var_dump($nbre) ?></li>
+    <li><?php var_dump($float) ?></li>
+    <li><?php var_dump($bool) ?></li>
     <li><?php var_dump($tab) ?></li>
-    <li><?php var_dump(null) ?></li>
-    <li><?php var_dump(new stdClass) ?></li>
+    <li><?php var_dump($null) ?></li>
+    <li><?php var_dump($obj) ?></li>
 </ul>

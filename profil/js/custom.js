93520c9b968b1197c1eb6a1ea351938159567817
mebale <noelmeb12@gmail.com>
2019-05-21 15:22:09 +0100

    images compressed

--- profil/js/custom.js
+++ profil/js/custom.js
@@ -1,14 +1,4 @@
-
-$(document).ready(function () {
-    let img = $('#my-pic');
-    TweenLite.from(img, 3, { x: -200, opacity: 0 });
-});
-/* WINDOW LOAD EVENTS */
-jQuery(window).load(function () {
-    "use strict";
-    jQuery("body").find('#site-loading').fadeOut(500);
-
-    // La carte
+window.onload = function () {
     var mymap = L.map('mapid').setView([0.4208681, 9.4418447], 15);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -17,6 +7,15 @@ jQuery(window).load(function () {
         accessToken: 'pk.eyJ1IjoibWFlc3Ryb2RpbWF0ZW8iLCJhIjoiY2pycmp4dTMxMWg2dTN5cHM5dDR4M204ZSJ9.oLlAJspzh3NIuxO-A7jLiw'
     }).addTo(mymap);
     L.marker([0.4208681, 9.4418447]).addTo(mymap);
+}
+$(document).ready(function () {
+    let img = $('#my-pic');
+    TweenLite.from(img, 3, { x: -200, opacity: 0 });
+});
+/* WINDOW LOAD EVENTS */
+jQuery(window).load(function () {
+    "use strict";
+    jQuery("body").find('#site-loading').fadeOut(200);
 });
 
 /* SUBMENU */
